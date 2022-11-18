b03166d872fffc12d021e6e6c94b33bd90c6042f
thstj <joonseok831@gmail.com>
2022-11-18 09:00:13 +0900

    221117

--- HellsysLibrary/Helpers/Helper.cs
+++ HellsysLibrary/Helpers/Helper.cs
@@ -6,12 +6,14 @@
         public static JsonHelper EPIJson { get; private set; }
         public static IniHelper EPIIni { get; private set; }
         public static RootHelper EPIRoot { get; private set; }
+        public static MathHelper EPIMath { get; private set; }
         static Helper()
         {
             EPILog = new LogHelper();
             EPIJson = new JsonHelper();
             EPIIni = new IniHelper();
             EPIRoot = new RootHelper();
+            EPIMath= new MathHelper();
         }
     }
 }
---
+++ HellsysLibrary/Maths/NumericPointT.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HellsysLibrary.Maths
+{
+    public class NumericPoint<T> : IComparable, ICloneable where T : IComparable
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+        public NumericPoint()
+        {
+        }
+
+        public NumericPoint(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Set(T x , T y)
+        {
+            X = x;
+            Y = y;
+        }
+        public NumericPoint<T> Swap() => new NumericPoint<T>() { X = Y, Y = X };
+        /// <summary>
+        /// 목적지의 포인트 값 복사
+        /// </summary>
+        /// <param name="dest"></param>
+        public void CopyTo(NumericPoint<T> dest)
+        {
+            dest.X = X;
+            dest.Y = Y;
+        }
+        /// <summary>
+        /// 출발지의 포인트 값 복사
+        /// </summary>
+        /// <param name="from"></param>
+        public void CopyFrom(NumericPoint<T> from)
+        {
+            X = from.X;
+            Y = from.Y;
+        }
+        public object Clone()=> this.MemberwiseClone();
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
