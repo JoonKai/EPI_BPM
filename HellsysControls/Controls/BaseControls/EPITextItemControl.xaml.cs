36bedef4979207bf77077cab76afb8f45e245ae9
thstj <joonseok831@gmail.com>
2023-01-31 03:56:12 +0900

    wef

--- HellsysControls/Controls/BaseControls/EPITextItemControl.xaml.cs
+++ HellsysControls/Controls/BaseControls/EPITextItemControl.xaml.cs
@@ -41,6 +41,18 @@ namespace HellsysControls.Controls.BaseControls
             set { SetValue(TBWidthProperty, value); }
         }
 
+
+        public string ItemText
+        {
+            get { return (string)GetValue(ItemTextProperty); }
+            set { SetValue(ItemTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ItemText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemTextProperty =
+            DependencyProperty.Register("ItemText", typeof(string), typeof(EPITextItemControl), new FrameworkPropertyMetadata("",FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
         #endregion
         public EPITextItemControl()
         {
