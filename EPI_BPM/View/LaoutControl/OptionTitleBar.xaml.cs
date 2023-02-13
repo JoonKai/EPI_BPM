d7e87031bbb9c824f1b20f61a910514d88a54d24
thstj <joonseok831@gmail.com>
2023-02-14 04:14:35 +0900

    111111

---
+++ EPI_BPM/View/LaoutControl/OptionTitleBar.xaml.cs
@@ -0,0 +1,51 @@
+﻿using HellsysLibrary.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EPI_BPM.View.LaoutControl
+{
+    /// <summary>
+    /// OptionTitleBar.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class OptionTitleBar : UserControl
+    {
+        public OptionTitleBar()
+        {
+            InitializeComponent();
+            btnExit.Click += BtnExit_Click;
+        }
+
+
+        private Window _parentWindow;
+
+
+        public Window ParentWindow
+        {
+            get
+            {
+                if (_parentWindow == null)
+                {
+                    _parentWindow = this.FindParent<Window>();
+                }
+                return _parentWindow;
+            }
+            set { _parentWindow = value; }
+        }
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            ParentWindow.Close();
+        }
+    }
+}
