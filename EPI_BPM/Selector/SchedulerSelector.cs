f4d4c751e762f491910bcaab2407a797d6e22288
thstj <joonseok831@gmail.com>
2023-02-20 04:18:22 +0900

    222222

---
+++ EPI_BPM/Selector/SchedulerSelector.cs
@@ -0,0 +1,26 @@
+﻿using EPI_BPM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EPI_BPM.Selector
+{
+    public class SchedulerSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            PMCategory pmCateogry = item as PMCategory;
+            FrameworkElement frameworkElement = container as FrameworkElement;
+
+            return (DataTemplate)frameworkElement.FindResource(
+                pmCateogry.ManufacturingStatus == "개발"
+                ?"PMScheduler1"
+                :"PMScheduler2"
+                );
+        }
+    }
+}
